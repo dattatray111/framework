e520f027d5d66c7bd29f74df0bb7e9112e725642
dattatray <dattatray111@gmail.com>
2020-05-31 13:57:26 +0530

    changes for compiler plugin error

--- framework/Flipkart/target/html/report.js
+++ framework/Flipkart/target/html/report.js
@@ -7,7 +7,7 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 63807754700,
+  "duration": 22792091000,
   "status": "passed"
 });
 formatter.scenario({
@@ -53,43 +53,43 @@ formatter.match({
   "location": "SmokeSuite.user_navigate_to_Flipkart()"
 });
 formatter.result({
-  "duration": 26924397300,
+  "duration": 9489495000,
   "status": "passed"
 });
 formatter.match({
   "location": "SmokeSuite.user_enter_username()"
 });
 formatter.result({
-  "duration": 1561451400,
+  "duration": 363245500,
   "status": "passed"
 });
 formatter.match({
   "location": "SmokeSuite.user_enter_password()"
 });
 formatter.result({
-  "duration": 1148562400,
+  "duration": 146868600,
   "status": "passed"
 });
 formatter.match({
   "location": "SmokeSuite.click_on_sign_in_button()"
 });
 formatter.result({
-  "duration": 313037500,
+  "duration": 91520200,
   "status": "passed"
 });
 formatter.match({
   "location": "SmokeSuite.user_should_be_logged_in()"
 });
 formatter.result({
-  "duration": 3326200,
+  "duration": 15299500,
   "status": "passed"
 });
 formatter.after({
-  "duration": 5967799100,
+  "duration": 226597300,
   "status": "passed"
 });
 formatter.after({
-  "duration": 11181613300,
+  "duration": 2899595200,
   "status": "passed"
 });
 });
